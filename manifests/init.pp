8adb6667cf877e1357cd0c1c1d3065094ead5521
Philipp Borgers <borgers@mi.fu-berlin.de>
2015-10-28 13:49:24 +0100

    double conntrack_max value

--- manifests/init.pp
+++ manifests/init.pp
@@ -70,6 +70,6 @@ class vpn03(
   sysctl { 'net.netfilter.nf_conntrack_generic_timeout': value => '120' }
   sysctl { 'net.ipv4.netfilter.ip_conntrack_generic_timeout': value => '120' }
   # double nf_conntrack_max default value
-  sysctl { 'net.netfilter.nf_conntrack_max': value => '65536' }
-  sysctl { 'net.nf_conntrack_max': value => '65536' }
+  sysctl { 'net.netfilter.nf_conntrack_max': value => '131072' }
+  sysctl { 'net.nf_conntrack_max': value => '131072' }
 }
